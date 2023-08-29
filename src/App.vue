fd44e92b87890737db5072f59295d25e83d29209
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-08-29 10:14:11 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container">
+    a
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    width: 100%;
+    height: 100vh;
+    background-color: #232323;
+  }
+</style>
+
+<!-- Buttons #413f3f -->
